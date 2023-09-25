b948fce960575510397e6a0795aad51461efa6e4
Tasondock Durin <57775017+the-prince237@users.noreply.github.com>
2023-09-25 01:38:53 +0100

    Update main.ts
    Adding jemy-fashion vercel url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://jemy-fashion.vercel.app',
   });
   await app.listen(3004);
 }

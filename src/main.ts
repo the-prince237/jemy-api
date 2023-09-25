31149e318cbd0dd61346a6c7bab77cfd8640f22b
Tasondock Durin <57775017+the-prince237@users.noreply.github.com>
2023-09-25 01:42:20 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: 'https://jemy-fashion.vercel.app' });
   app.enableCors({
     origin: 'https://jemy-fashion.vercel.app',
   });
